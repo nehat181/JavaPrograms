001646d0c45473ea3b85baad9dd6470c51b79f31
nehatprovar <nehat181@gmail.com>
2020-04-23 01:20:59 +0530

    push the changes

--- src/ThoughtWorks/SmallestSubarray.java
+++ src/ThoughtWorks/SmallestSubarray.java
@@ -23,9 +23,7 @@ public class SmallestSubarray {
 
 			}
 
-
-
-		}  
+	}  
 
 
 		return min;
---
+++ src/ThoughtWorks/testpush.java
@@ -0,0 +1,10 @@
+package ThoughtWorks;
+
+public class testpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
